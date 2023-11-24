26d510158af8e59fb9a39e46057e8944ee7d1a7a
Adarsh4775 <137145969+Adarsh4775@users.noreply.github.com>
2023-11-24 14:30:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+
+ let unDiv = document.createElement("div");
+  document.body.appendChild(unDiv);
+
+ let unOrder = document.createElement("ul");
+  unOrder.id = "band";
+
+bands.forEach((band) =>{
+
+	 let mBand = band.replace(/\b(?:a|an|the)\b/gi, '').trim();
+	  let unLii = document.createElement("li");
+	   unLii.innerText = mBand;
+	   unOrder.appendChild(unLii);
+	   unDiv.appendChild(unOrder);
+	   
+   });
